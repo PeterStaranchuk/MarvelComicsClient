09aef987aafd8c8242117b31c5954992bcde71b5
peterstaranchuk <petr.staranchuk@gmail.com>
2021-08-01 19:13:53 +0300

    Add fragment with catalog

--- app/heroes_catalog/src/main/java/com/peterstaranchuk/heroes_catalog/HeroesCatalogFragment.kt
+++ app/heroes_catalog/src/main/java/com/peterstaranchuk/heroes_catalog/HeroesCatalogFragment.kt
@@ -1,7 +1,16 @@
 package com.peterstaranchuk.heroes_catalog
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import com.peterstaranchuk.common.BaseFragment
+import com.peterstaranchuk.heroes_catalog.databinding.FragmentCatalogBinding
 
 class HeroesCatalogFragment : BaseFragment() {
 
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        val binding = FragmentCatalogBinding.inflate(LayoutInflater.from(context), container, false)
+        return binding.root
+    }
 }
