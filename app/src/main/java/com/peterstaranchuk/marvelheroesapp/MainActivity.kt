9d8deb326255cb54ab01a937b915f9d01c7f7ce4
peterstaranchuk <petr.staranchuk@gmail.com>
2021-08-01 19:13:32 +0300

    Add and connect navigation graph

--- app/src/main/java/com/peterstaranchuk/marvelheroesapp/MainActivity.kt
+++ app/src/main/java/com/peterstaranchuk/marvelheroesapp/MainActivity.kt
@@ -1,18 +1,13 @@
 package com.peterstaranchuk.marvelheroesapp
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import com.peterstaranchuk.marvelheroesapp.ui.theme.MarvelHeroesAppTheme
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
 
     }
 }
