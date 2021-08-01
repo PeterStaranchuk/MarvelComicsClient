c3a0b8e09517ff4fcddab3c856e7439bc793caca
peterstaranchuk <petr.staranchuk@gmail.com>
2021-08-01 20:31:28 +0300

    Add presentation class for comics and mapper

---
+++ app/heroes_catalog/src/main/java/com/peterstaranchuk/heroes_catalog/ComicsModelToPresentationMapper.kt
@@ -0,0 +1,12 @@
+package com.peterstaranchuk.heroes_catalog
+
+import com.peterstaranchuk.common.Mapper
+
+class ComicsModelToPresentationMapper : Mapper<ComicsModel, ComicsPresentation> {
+    override fun map(from: ComicsModel): ComicsPresentation {
+        return ComicsPresentation(
+            id = from.id,
+            imageUrl = from.imageUrl
+        )
+    }
+}
---
+++ app/heroes_catalog/src/main/java/com/peterstaranchuk/heroes_catalog/ComicsPresentation.kt
@@ -0,0 +1,3 @@
+package com.peterstaranchuk.heroes_catalog
+
+data class ComicsPresentation(val id : Long, val imageUrl : String)
