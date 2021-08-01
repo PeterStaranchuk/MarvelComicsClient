c5428872e84460da6e1fd0982a5ed76fcae85dd8
peterstaranchuk <petr.staranchuk@gmail.com>
2021-08-01 17:22:04 +0300

    Initial commit

---
+++ app/src/main/java/com/peterstaranchuk/marvelheroesapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.peterstaranchuk.marvelheroesapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.peterstaranchuk.marvelheroesapp.ui.theme.MarvelHeroesAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
