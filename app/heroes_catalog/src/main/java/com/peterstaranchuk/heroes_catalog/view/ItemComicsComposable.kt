5dfd3191356aca3d4e8b118f0c0d78519c46227f
peterstaranchuk <petr.staranchuk@gmail.com>
2021-08-04 19:05:40 +0300

    Use palette to set dominant color of image

--- app/heroes_catalog/src/main/java/com/peterstaranchuk/heroes_catalog/view/ItemComicsComposable.kt
+++ app/heroes_catalog/src/main/java/com/peterstaranchuk/heroes_catalog/view/ItemComicsComposable.kt
@@ -1,15 +1,20 @@
 package com.peterstaranchuk.heroes_catalog.view
 
 import android.content.res.Configuration
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.net.Uri
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
@@ -18,34 +23,60 @@ import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import coil.compose.rememberImagePainter
+import androidx.core.content.ContextCompat
+import androidx.palette.graphics.Palette
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.peterstaranchuk.common.HttpKeys
 import com.peterstaranchuk.heroes_catalog.BuildConfig
 import com.peterstaranchuk.heroes_catalog.R
 import com.peterstaranchuk.heroes_catalog.presentation.ComicsPresentation
 
+
 @Composable
 fun ItemComics(comicsPresentation: ComicsPresentation) {
     var isExpanded by remember { mutableStateOf(false) }
+    val context = LocalContext.current
+    var color by remember { mutableStateOf(Color.White)}
 
     Column(Modifier.clickable {
         isExpanded = !isExpanded
-    }) {
+    }.background(color)) {
         val url = Uri.parse(comicsPresentation.imageUrl)
             .buildUpon()
             .appendQueryParameter(HttpKeys.API_KEY, BuildConfig.API_KEY)
             .appendQueryParameter(HttpKeys.TIMESTAMP, BuildConfig.TIMESTAMP)
             .appendQueryParameter(HttpKeys.HASH, BuildConfig.HASH.lowercase())
             .build()
-        Spacer(modifier = Modifier.height(2.dp))
-        Image(
-            painter = rememberImagePainter(data = url.toString()),
-            contentScale = ContentScale.Crop,
-            contentDescription = null,
-            modifier = Modifier
-                .aspectRatio(1f)
-                .defaultMinSize(40.dp, 40.dp)
-        )
+
+        Spacer(modifier = Modifier
+                .height(2.dp))
+
+        val bitmapState = remember { mutableStateOf<Bitmap?>(null) }
+        Glide.with(LocalContext.current)
+                .asBitmap()
+                .load(url)
+                .into(
+                object : CustomTarget<Bitmap>() {
+                    override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                        bitmapState.value = resource
+                        val colorInt = Palette.from(resource).generate().getDominantColor(ContextCompat.getColor(context, android.R.color.white))
+                        color = Color(colorInt)
+                    }
+
+                    override fun onLoadCleared(placeholder: Drawable?) {}
+                })
+
+        bitmapState.value?.let {
+            Image(
+                    bitmap = it.asImageBitmap(),
+                    contentScale = ContentScale.Crop,
+                    contentDescription = null,
+                    modifier = Modifier
+                            .aspectRatio(1f)
+                            .defaultMinSize(40.dp, 40.dp))
+        }
 
         Text(text = buildAnnotatedString {
             withStyle(style = SpanStyle(color = Color.Black)) {
